c6f70c05885c3be0b04eb37eee0a7c9e8a24243c
gabriel <gabrielguimasr@gmail.com>
2026-05-08 20:15:36 -0300

    [FEAT] Finished + Added Experimental Resutls

--- search.py
+++ search.py
@@ -1,3 +1,4 @@
+import os
 from shutil import move
 from typing import List, Tuple, Optional, Dict
 import time
@@ -74,97 +75,32 @@ def other(player: int) -> int:
 # -----------------------------------------------------------------------------
 # ÚNICO PONTO A SER IMPLEMENTADO PELOS ALUNOS
 # -----------------------------------------------------------------------------
-def choose_move(board: List[List[int]], turn: int, config: Dict) -> Tuple[int, Dict]:
-    """
-    Decide a coluna (0..6) para jogar agora.
+def registrar_jogada(modeloA,modeloB,estados_visitados, tempo_jogada, profundidade, arquivo="data/registro_partida_1.csv"):
+    precisa_cabecalho = True
+    try:
+        with open(arquivo, 'r', encoding='utf-8') as f:
+            precisa_cabecalho = False
+    except FileNotFoundError:
+        pass 
 
-    Parâmetros:
-      - board: matriz 6x7 com valores {0,1,2}
-      - turn: 1 ou 2
-      - config: {"max_time_ms": int, "max_depth": int}
-
-    Retorna:
-      - col: int (0..6)
-    """
-    max_time_ms = int(config.get("max_time_ms"))
-    max_depth = int(config.get("max_depth"))
-    turn = int(turn)
-
-    print(f"AI choose_move called with max_time_ms={max_time_ms}, max_depth={max_depth}, player={turn}")
-    
-    start = time.time()
-
-    # Função auxiliar para checar tempo decorrido   
-    def time_exceeded():
-        return max_time_ms > 0 and (time.time() - start) * 1000.0 >= max_time_ms
-    
-    legal = valid_moves(board)
-
-    best_move = 0
-    if not legal:
-        # Sem jogadas: devolve 0 por convenção (servidor lida com isso)
-        return best_move
-    
-    prefered_order = [3, 2, 4] in legal and [3, 2, 4] or legal
-    
-    best_move = random.choice(prefered_order)  # Inicializa com uma jogada aleatória válida
-    
-    # min-max padrão
-    # _, move = min_max(
-    #     max_turn=True, 
-    #     player=turn, 
-    #     turn=turn, 
-    #     board=board, 
-    #     legal=legal, 
-    #     time_check=time_exceeded,
-    #     max_depth=max_depth
-    # )
-    
-    # alpha-beta padrão
-    # _, move = alpha_beta(
-    #     max_turn=True, 
-    #     player=turn, 
-    #     turn=turn, 
-    #     board=board, 
-    #     legal=legal, 
-    #     time_check=time_exceeded,
-    #     max_depth=max_depth
-    # )
-    
-    #interactive deepening
-    for depth in range(1, max_depth + 1):
-        if time_exceeded():
-            print("Time exceeded before starting depth", depth)
-            break
+    with open(arquivo, 'a', encoding='utf-8') as f:
+        if precisa_cabecalho:
+            f.write("ModeloA,ModeloB,Estados Visitados,Tempo por Jogada,Profundidade\n")
         
-        #com min-max
-        # _, move = min_max(
-        #     max_turn=True, 
-        #     player=turn, 
-        #     turn=turn, 
-        #     board=board, 
-        #     legal=legal, 
-        #     time_check=time_exceeded,
-        #     max_depth=depth
-        # )
-        
-        #com alpha-beta
-        _, move = alpha_beta(
-            max_turn=True, 
-            player=turn, 
-            turn=turn, 
-            board=board, 
-            legal=legal, 
-            time_check=time_exceeded,
-            max_depth=depth
-        )
-        
-        # Só atualiza a melhor jogada se a busca terminou ANTES do tempo acabar
-        if not time_exceeded() and move in legal:
-            best_move = move
-            
-    return best_move
+        estados_str = str(estados_visitados)
+        tempo_str = str(tempo_jogada)
+        profundidade_str = str(profundidade)
 
+        if ',' in estados_str:
+            estados_str = f'"{estados_str}"'
+        if ',' in tempo_str:
+            tempo_str = f'"{tempo_str}"'
+        if ',' in profundidade_str:
+            profundidade_str = f'"{profundidade_str}"'
+
+        linha = f"{modeloA},{modeloB},{estados_str},{tempo_str},{profundidade_str}\n"
+        
+        f.write(linha)
 
 def evaluate(board: List[List[int]], player: int) -> int:
     opponent = other(player)
@@ -197,7 +133,11 @@ def evaluate(board: List[List[int]], player: int) -> int:
 
     return score
 
-def min_max(max_turn: bool = False, player: int = 1, turn: int = 1, board: List[List[int]] = None, legal: List[int] = None, time_check=None, max_depth: int = 1) -> Tuple[int, int]:
+def min_max(max_turn: bool = False, player: int = 1, turn: int = 1, board: List[List[int]] = None, 
+            legal: List[int] = None, time_check=None, max_depth: int = 1, nodes: List[int] = None) -> Tuple[int, int]:
+
+    if nodes is not None:
+        nodes[0] += 1
 
     if time_check():
         return evaluate(board, player), (legal[0] if legal else 0)
@@ -221,7 +161,8 @@ def min_max(max_turn: bool = False, player: int = 1, turn: int = 1, board: List[
             board=new_board, 
             legal=valid_moves(new_board),
             time_check=time_check,
-            max_depth=(max_depth - 1)
+            max_depth=(max_depth - 1),
+            nodes=nodes
         )
                 
         if max_turn:
@@ -235,7 +176,12 @@ def min_max(max_turn: bool = False, player: int = 1, turn: int = 1, board: List[
     
     return best_score, best_move
 
-def alpha_beta(max_turn: bool = False, player: int = 1, turn: int = 1, board: List[List[int]] = None, legal: List[int] = None, time_check=None, max_depth: int = 1, alpha: float = -math.inf, beta: float = math.inf) -> Tuple[int, int]:
+def alpha_beta(max_turn: bool = False, player: int = 1, turn: int = 1, board: List[List[int]] = None, legal: List[int] = None, 
+               time_check=None, max_depth: int = 1, alpha: float = -math.inf, beta: float = math.inf,nodes: List[int] = None) -> Tuple[int, int]:
+    
+    if nodes is not None:
+        nodes[0] += 1
+    
     if time_check():
         return evaluate(board, player), (legal[0] if legal else 0)
 
@@ -260,7 +206,8 @@ def alpha_beta(max_turn: bool = False, player: int = 1, turn: int = 1, board: Li
             time_check=time_check,
             max_depth=(max_depth - 1),
             alpha=alpha,
-            beta=beta
+            beta=beta,
+            nodes=nodes
         )
             
             
@@ -286,7 +233,111 @@ def alpha_beta(max_turn: bool = False, player: int = 1, turn: int = 1, board: Li
         
     return best_score, best_move
     
-                
+def choose_move(board: List[List[int]], turn: int, config: Dict) -> Tuple[int, Dict]:
+    """
+    Decide a coluna (0..6) para jogar agora.
+
+    Parâmetros:
+      - board: matriz 6x7 com valores {0,1,2}
+      - turn: 1 ou 2
+      - config: {"max_time_ms": int, "max_depth": int}
+
+    Retorna:
+      - col: int (0..6)
+    """
+    max_time_ms = int(config.get("max_time_ms"))
+    max_depth = int(config.get("max_depth"))
+    turn = int(turn)
+
+    print(f"AI choose_move called with max_time_ms={max_time_ms}, max_depth={max_depth}, player={turn}")
+    
+    start = time.time()
+
+    # Função auxiliar para checar tempo decorrido   
+    def time_exceeded():
+        return max_time_ms > 0 and (time.time() - start) * 1000.0 >= max_time_ms
+    
+    legal = valid_moves(board)
+
+    nodes_visited = [0]
+    best_move = 0
+    
+    if not legal:
+        # Sem jogadas: devolve 0 por convenção (servidor lida com isso)
+        return best_move
+    
+    prefered_order = [3, 2, 4] in legal and [3, 2, 4] or legal
+    
+    best_move = random.choice(prefered_order)  # Inicializa com uma jogada aleatória válida
+    
+    #min-max padrão
+    # _, move = min_max(
+    #     max_turn=True, 
+    #     player=turn, 
+    #     turn=turn, 
+    #     board=board, 
+    #     legal=legal, 
+    #     time_check=time_exceeded,
+    #     max_depth=max_depth,
+    #    nodes=nodes_visited   
+    # )
+    
+    # alpha-beta padrão
+    # _, move = alpha_beta(
+    #     max_turn=True, 
+    #     player=turn, 
+    #     turn=turn, 
+    #     board=board, 
+    #     legal=legal, 
+    #     time_check=time_exceeded,
+    #     max_depth=max_depth,
+    #     nodes=nodes_visited
+    # )
+    
+    #interactive deepening
+    for depth in range(1, max_depth + 1):
+        if time_exceeded():
+            print("Time exceeded before starting depth", depth)
+            break
+        
+    #     #com min-max
+    #     # _, move = min_max(
+    #     #     max_turn=True, 
+    #     #     player=turn, 
+    #     #     turn=turn, 
+    #     #     board=board, 
+    #     #     legal=legal, 
+    #     #     time_check=time_exceeded,
+    #     #     max_depth=depth
+    #     #     nodes=nodes_visited
+    #     # )
+        
+    #     # #com alpha-beta
+        _, move = alpha_beta(
+            max_turn=True, 
+            player=turn, 
+            turn=turn, 
+            board=board, 
+            legal=legal, 
+            time_check=time_exceeded,
+            max_depth=depth,
+            nodes=nodes_visited
+        )
+        
+    #     # Só atualiza a melhor jogada se a busca terminou ANTES do tempo acabar
+        if not time_exceeded() and move in legal:
+            best_move = move
+    
+    #Usado para os experimentos
+    #tempo_total = time.time() - start       
+    #registrar_jogada("MinMax","Random", nodes_visited[0], tempo_total, max_depth)
+    #registrar_jogada("MinMax","Alpha-Beta", nodes_visited[0], tempo_total, max_depth)
+    #registrar_jogada("Alpha-Beta","MinMax", nodes_visited[0], tempo_total, max_depth)
+    #registrar_jogada("Alpha-Beta","Iterative Deepening", nodes_visited[0], tempo_total, max_depth)
+    #registrar_jogada("Iterative Deepening","Alpha-Beta", nodes_visited[0], tempo_total, max_depth)
+    #registrar_jogada("Iterative Deepening","Humano", nodes_visited[0], tempo_total, max_depth)
+
+    return best_move         
         
 def choose_move_randomly(board: List[List[int]], turn: int, config: Dict) -> Tuple[int, Dict]:
     max_time_ms = int(config.get("max_time_ms"))
